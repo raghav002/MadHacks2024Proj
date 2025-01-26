e6827086515deb3db66f69e945b6701037206e1c
Raghav Jindal <raghavjindal2002@gmail.com>
2025-01-26 15:03:59 -0600

    Stuff

--- motionmodel.py
+++ motionmodel.py
@@ -4,20 +4,20 @@
 from motiondatacollection import collect_data
 from motionpreprocessing import preprocess_data
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, Flatten
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.utils import to_categorical
+from tensorflow import Sequential
+from tensorflow import LSTM, Dense, Dropout, Flatten
+from tensorflow import TensorBoard
+from tensorflow import to_categorical
 import os
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, TimeDistributed, Flatten
-from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow import ResNet50
+from tensorflow import Model, Sequential
+from tensorflow import LSTM, Dense, Dropout, TimeDistributed, Flatten
+from tensorflow import GlobalAveragePooling2D
 
 from config import ACTIONS
 import numpy as np
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import load_model
+from tensorflow import load_model
 
 def create_model():
     model = Sequential()
